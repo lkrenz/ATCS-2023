8268e75b6eb007001fc226c191c28fb61373e42a
lkrenz <liamkrenz100@gmail.com>
2023-12-06 21:10:48 -0800

    Found images, and added building's money generation and purchase process. Need to fix issue with button to purchase

--- final_project/game.py
+++ final_project/game.py
@@ -43,46 +43,123 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.counter = 0
-        self.barbell = Barbell(50, self.height / 2 - 200, "final_project/assets/barbell.png")
+        self.barbell = Barbell(50, self.height / 2 - 200, "assets/barbell.png")
 
         self.fsm = FSM()
 
+        self.gym = gym_1()
+        self.seconds_timer = 0  # Timer for tracking seconds
+
+        # Button parameters
+        self.button_width = 100
+        self.button_height = 50
+        self.button_color = (0, 255, 0)  # Green button color
+        self.button_rect = pygame.Rect(self.width - self.button_width, self.height / 2 - self.button_height / 2,
+                                       self.button_width, self.button_height)
+
     def run(self):
-        font = pygame.font.Font(None, 36)
+        self.font = pygame.font.Font(None, 36)
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.barbell.is_clicked(event.pos):
-                        self.increment_counter()
-                        print(f"Counter value: {self.get_counter_value()}")
-                        self.barbell.start_outline_effect()  # Trigger the outline effect
-
-            self.barbell.update()  # Update the barbell's state
-
-            self.screen.fill((255, 255, 255))
-
-            # Draw the left section with the clickable image
-            pygame.draw.rect(self.screen, (200, 200, 200), (0, 0, self.width // 2, self.height))
-            self.barbell.draw(self.screen)
-
-            # Draw the right section (empty for now)
-            pygame.draw.rect(self.screen, (255, 255, 255), (self.width // 2, 0, self.width // 2, self.height))
-
-            # Display the counter at the top of the screen
-            counter_text = font.render(f"Counter: {self.get_counter_value()}", True, (0, 0, 0))
-            self.screen.blit(counter_text, (10, 10))
-
-            pygame.display.flip()
-            self.clock.tick(60)
+            self.regular_mode()
+            self.seconds_timer += 1  # Increment the timer
+            if self.seconds_timer == 60:  # Increment counter every 60 frames (1 second)
+                self.increment_counter_by_money()
+                self.seconds_timer = 0  # Reset the timer
 
     def increment_counter(self):
         self.counter += 1
 
+    def increment_counter_by_money(self):
+        money_generated = self.gym.calculate_money()
+        self.counter += money_generated
+        print(f"Counter value: {self.get_counter_value()} (+{money_generated} money)")
+
+
     def get_counter_value(self):
         return self.counter
+    
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Left mouse button
+                    if self.button_rect.collidepoint(event.pos):
+                        self.purchase_building(0, 1)
+                        print('1')
+
+    def purchase_building(self, building_type, number):
+        cost = self.gym.get_cost(building_type, number)
+        if self.get_counter_value() >= cost:
+            self.increment_counter(-cost)  # Deduct the cost from the counter
+            self.gym.purchase_building(building_type, number)
+    
+    def regular_mode(self):
+        # state = self.fsm.get_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if self.barbell.is_clicked(event.pos):
+                    self.increment_counter()
+                    print(f"Counter value: {self.get_counter_value()}")
+                    self.barbell.start_outline_effect()  # Trigger the outline effect
+
+
+        self.barbell.update()  # Update the barbell's state
+
+        self.screen.fill((255, 255, 255))
+
+        # Draw the left section with the clickable image
+        pygame.draw.rect(self.screen, (200, 200, 200), (0, 0, self.width // 2, self.height))
+        self.barbell.draw(self.screen)
+
+        # Draw the right section (empty for now)
+        pygame.draw.rect(self.screen, (255, 255, 255), (self.width // 2, 0, self.width // 2, self.height))
+
+        # Display the counter at the top of the screen
+        counter_text = self.font.render(f"Counter: {self.get_counter_value()}", True, (0, 0, 0))
+        self.screen.blit(counter_text, (10, 10))
+
+        # Draw the button on the right side of the screen
+        pygame.draw.rect(self.screen, self.button_color, self.button_rect)
+        button_text = self.font.render("Add Building", True, (255, 255, 255))
+        self.screen.blit(button_text, (self.width - self.button_width + 10, self.height / 2 - self.button_height / 2 + 10))
+
+        pygame.display.flip()
+        self.clock.tick(60)
+
+        
+
+class gym_1:
+    def __init__(self) :
+        self.images = []
+        self.images.append(pygame.image.load("assets/weight.png"))
+        self.images.append(pygame.image.load("assets/squat rack.png"))
+        self.images.append(pygame.image.load("assets/bench.png"))
+        # self.images[3] = pygame.image.load("assets/weight.png")
+
+        # [Amount of building, initial price of building, strength generated per second]
+        self.buildings = []
+        self.buildings.append([0, 100, 10])
+        self.buildings.append([0, 1000, 100])
+        self.buildings.append([0, 100000, 1000])
+        self.buildings.append([0, 10000000, 10000])
+
+    def calculate_money(self) :
+        money = 0
+        for i in self.buildings:
+            money += (i[0] * i[2])
+        return money
+    
+    def get_cost(self, building, number) :
+        return self.buildings[building][1] * number
+
+    def purchase_building(self, building, number) :
+        self.buildings[building][2] += number
 
 if __name__ == "__main__":
     game = Game()
